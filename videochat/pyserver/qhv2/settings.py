c4cd52d7b5852e004586321f1eab2c6fc0cc2e92
Pawel Cichon <pcichon@genb.com>
2022-04-07 00:22:39 +0200

    refactor

--- videochat/pyserver/qhv2/settings.py
+++ videochat/pyserver/qhv2/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'qhv2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
