172dd39b95e1a676bc1595ddbd9e7ceedd51a2a1
Pawel Cichon <pcichon@genb.com>
2022-04-07 20:36:13 +0200

    Update settings.py

--- console/pyserver/console/settings.py
+++ console/pyserver/console/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from distutils.util import strtobool
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
