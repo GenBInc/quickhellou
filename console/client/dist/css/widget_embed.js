0ad2665a55cae0da32492f15cd680acc15699513
Pawel Cichon <pcichon@genb.com>
2022-11-23 20:27:08 +0100

    css updates

---
+++ console/client/dist/css/widget_embed.js
@@ -0,0 +1,3 @@
+/*! For license information please see widget_embed.js.LICENSE.txt */
+!function(){"use strict";var e={r:function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}};e.r({})}();
+//# sourceMappingURL=widget_embed.js.map
